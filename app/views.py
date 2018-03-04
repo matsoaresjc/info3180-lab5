bdd1f8780451c011544c51bbbff265179863a6f2
Mat Soares <matthew.soares@mymona.uwi.edu>
2018-03-04 00:35:57 +0000

    edited views

--- app/views.py
+++ app/views.py
@@ -31,22 +31,24 @@ def about():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
-    if request.method == "POST":
+    if request.method == 'POST' and form.validate_on_submit():
         # change this to actually validate the entire form submission
         # and not just one field
-        if form.username.data:
+        
             # Get the username and password values from the form.
-
+            username = form.username.data
+            password = form.password.data
             # using your model, query database for a user based on the username
             # and password submitted
             # store the result of that query to a `user` variable so it can be
             # passed to the login_user() method.
-
+            user = UserProfile.query.filter_by(username = username,password=password).first()
             # get user id, load into session
             login_user(user)
 
             # remember to flash a message to the user
-            return redirect(url_for("home"))  # they should be redirected to a secure-page route instead
+            flash("Successfully logged in")
+            return redirect(url_for("secure_page"))  # they should be redirected to a secure-page route instead
     return render_template("login.html", form=form)
 
 
